060fa4f743d24f9ddd06853ba993cca7d4dd9a1a
kyrylozo <kyzltk@gmail.com>
2023-08-10 18:35:28 +0200

    Условные операторы. Использование "ToLower" и точки с запятой после "else"

---
+++ Lectures/05_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") // .ToLower позволяет реагировать на string не зависимо от регистра букв в любом положении
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else; // ";" после else позволяет, при условии TRUTH, выводить последовательно оба действия: TRUTH, затем FALSE
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}

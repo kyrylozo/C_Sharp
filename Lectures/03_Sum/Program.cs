9cb1f950f79fd67e90afba3feeb14ebe6002cdcb
kyrylozo <kyzltk@gmail.com>
2023-08-10 17:41:14 +0200

    Добавлена генерация случайных чисел

--- Lectures/03_Sum/Program.cs
+++ Lectures/03_Sum/Program.cs
@@ -1,5 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
-byte numberA = 66;
-byte numberB = 5;
+﻿
+int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9  (new Random().Next(min, max-1) ВСЕГДА ГЕРЕРИРУЕТ int!
+int numberB = new Random().Next(1, 10);
+Console.Write ("Первое число ");
+Console.WriteLine(numberA);
+Console.Write ("Второе число ");
+Console.WriteLine(numberB);
+Console.Write ("Сумма ");
 Console.WriteLine(numberA + numberB);
 

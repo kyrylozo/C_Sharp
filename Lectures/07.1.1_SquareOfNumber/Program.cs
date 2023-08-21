552e10187b179ce8464f4bcbaf420d6246f233fd
kyrylozo <kyzltk@gmail.com>
2023-08-21 13:32:55 +0200

    if else operators

---
+++ Lectures/07.1.1_SquareOfNumber/Program.cs
@@ -0,0 +1,9 @@
+﻿// Принимает число и выдает его квадрат
+
+Console.WriteLine("Enter number: ");
+// string StrNum = Console.ReadLine();
+// int Num = Convert.ToInt32(StrNum);
+int Num = Convert.ToInt32(Console.ReadLine());
+int y = Num * Num;
+Console.WriteLine(y);
+
---
+++ Lectures/07.1.2.CheckSquare/Program.cs
@@ -0,0 +1,17 @@
+﻿// Принимаепт два числа и проверчяет, является ли первое квадратом второго
+
+Console.WriteLine("Enter the 1st number: ");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the 2d number: ");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+int Square = Num2 * Num2;
+if(Square == Num1)
+{
+    Console.WriteLine ($"{Num1} равно квадрату {Num2}");
+}
+else
+{
+    Console.WriteLine ($"Число {Num1} не является квадратом {Num2}");
+}
+
+

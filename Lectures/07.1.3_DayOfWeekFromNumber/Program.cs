d0df82bcb7a18e5f2ec1727e7166698408cd45a2
kyrylozo <kyzltk@gmail.com>
2023-08-22 10:38:22 +0200

    Выводит значение дня недели по заданному номеру

---
+++ Lectures/07.1.3_DayOfWeekFromNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿// Выводит название дня недели по заданному номеру
+
+Console.WriteLine("Enter the number from 1 to 7");
+int DayNum = Convert.ToInt32(Console.ReadLine());
+if (DayNum == 1) 
+{
+    Console.WriteLine("Monday");
+}
+else if (DayNum == 2) 
+{
+    Console.WriteLine("Tuesday");
+}
+else if (DayNum == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+else if (DayNum == 4)
+{
+    Console.WriteLine("Thursday");
+}
+else if (DayNum == 5) 
+{
+    Console.WriteLine("Friday");
+}
+else if (DayNum == 6)
+{
+    Console.WriteLine("Saturday");
+}
+else if (DayNum == 7)
+{
+    Console.WriteLine("Sunday");
+}
+else //if (DayNum < 1 || DayNum > 7) 
+{
+    Console.WriteLine("Enter correct number");
+}

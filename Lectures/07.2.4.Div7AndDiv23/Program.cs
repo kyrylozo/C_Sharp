838232210dc1dec291f834256c99d2bd3f758e81
kyrylozo <kyzltk@gmail.com>
2023-08-23 16:55:20 +0200

    Принимает число и проверяет, кратно ли оно одновременно 7 и 2

---
+++ Lectures/07.2.4.Div7AndDiv23/Program.cs
@@ -0,0 +1,12 @@
+﻿// Принимает число и проверяет, кратно ли оно одновременно 7 и 2
+
+System.Console.WriteLine("Enter the number: ");
+int n = int.Parse(Console.ReadLine());
+if (n % 7 == 0 && n % 2 == 0)
+{
+    System.Console.WriteLine($"Number {n} can be devided either 7 or 2");
+}
+else
+{
+    System.Console.WriteLine($"Number {n} can not be divided to 7 and 2");
+}

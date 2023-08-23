810a774f9b6515439e47eba43295270cdaef7042
kyrylozo <kyzltk@gmail.com>
2023-08-23 16:12:50 +0200

    Выводит случайное трехзначное число и удаляет 2-ю цифру этого числа

---
+++ Lectures/07.2.2.Del2dNumFromFirure/Program.cs
@@ -0,0 +1,8 @@
+﻿// Выводит случайное трехзначное число и удаляет 2-ю цифру этого числа
+
+int Num = new Random().Next(100, 1000);
+int a = Num / 100 * 10;
+int b = Num % 10;
+int R = a + b;
+System.Console.WriteLine($"Rundom number = {Num}");
+System.Console.WriteLine(R);

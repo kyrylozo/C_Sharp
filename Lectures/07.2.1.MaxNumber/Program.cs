f4fca28926a772d2f63118d7f3a8d627b23b26de
kyrylozo <kyzltk@gmail.com>
2023-08-23 15:58:25 +0200

    Выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа

---
+++ Lectures/07.2.1.MaxNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
+
+int N = new Random().Next(10, 100);
+int a = N / 10;
+int b = N % 10;
+System.Console.WriteLine(N);
+System.Console.WriteLine(a);
+System.Console.WriteLine(b);
+if (a > b)
+{
+    System.Console.WriteLine($"{a} > {b}");
+}
+

5ceb426297183323566b55b6594c923b6e4453bc
kyrylozo <kyzltk@gmail.com>
2023-08-30 11:07:23 +0200

    Принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N

---
+++ Lectures/07.3.3.SqrsFrom1ToN/Program.cs
@@ -0,0 +1,9 @@
+﻿// Принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N.
+
+System.Console.Write("Enter the number: ");
+double N = double.Parse(Console.ReadLine());
+while (N > 0)
+{
+    System.Console.WriteLine($"Квадрат числа {N} = {Math.Pow(N,2)}");
+    N -= 1;
+}
